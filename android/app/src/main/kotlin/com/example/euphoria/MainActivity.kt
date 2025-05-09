11c1531699f0a53159e3054b8a4ebeda6fe2e711
aaidahartini <81534872+aaidahartini@users.noreply.github.com>
2025-05-09 09:33:41 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/euphoria/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.euphoria
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+//Import necessary FLutter package to use widgets and function
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Euphoria',
+      theme: ThemeData(primaryColor: Colors.brown),
+      home: const MyHomePage(title: "Demo Home"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: const Center(child: Text("Hellow, aaida!!")),
+    );
+  }
+}
